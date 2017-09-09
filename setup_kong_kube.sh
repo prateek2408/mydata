e686d25644e96c22ba1ed8c984620c8ce1de0a62
prateek2408 <prateek.khushalani@gmail.com>
2017-09-09 21:38:20 +0530

    Create setup_kong_kube.sh

---
+++ setup_kong_kube.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+echo "---> Installing Kubeadm"
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm
+
+echo "---> Install Kubeadm complete"
+echo "---> Setup kubeadm"
+kubeadm init
+export KUBECONFIG=/etc/kubernetes/admin.conf 
+kubectl taint nodes --all node-role.kubernetes.io/master-
+echo "---> Kubeadm completed with master node tainted"
+
+echo "Setting up Weave network"
+export kubever=$(kubectl version | base64 | tr -d '\n')
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
+sleep 120 
+
+echo "Setting up Kong HTTP Api Gateway"
+git clone https://github.com/Mashape/kong-dist-kubernetes.git
+cd kong-dist-kubernetes/
+kubectl create -f postgres.yaml
+sleep 50
+kubectl create -f kong_migration_postgres.yaml
+sleep 50
+kubectl create -f kong_postgres.yaml
+sleep 50
+
+echo "KUBECONFIG=/etc/kubernetes/admin.conf" >> /etc/environment
